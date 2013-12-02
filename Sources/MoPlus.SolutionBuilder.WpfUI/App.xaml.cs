cd8360dfe84b49ff9f2d774987d65dee31dee840
Matthijs ter Woord <matthijs@prodoc.nl>
2013-12-02 08:57:28 +0100

    small fixes: - mssql datetime2 support - overall exception catcher

--- Sources/MoPlus.SolutionBuilder.WpfUI/App.xaml.cs
+++ Sources/MoPlus.SolutionBuilder.WpfUI/App.xaml.cs
@@ -23,5 +23,16 @@ namespace MoPlus.SolutionBuilder.WpfUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
     }
 }
